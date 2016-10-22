01aeafb478f61a8a47cc87ecc0f72c2ba527dfb7
Ryan Schmidt <rms@rms80.com>
2016-10-22 11:32:45 -0400

    copied from frame3Sharp

---
+++ geometry3Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using g3;
+
+namespace frame3Test
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+			SimpleMeshBuilder builder = new SimpleMeshBuilder();
+			var readResult = StandardMeshReader.ReadFile("c:\\scratch\\temp.obj", new ReadOptions(), builder);
+			if (readResult.Item1 != ReadResult.Ok)
+				throw new Exception("fuck");
+
+			var writeResult = StandardMeshWriter.WriteFile("c:\\scratch\\temp_new.obj",
+				builder.Meshes.Cast<IMesh>().ToList(), new WriteOptions());
+			if (writeResult.Item1 != WriteResult.Ok)
+				throw new Exception("fuck");
+        }
+    }
+}
